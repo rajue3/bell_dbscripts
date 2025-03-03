861e5cb02a603afaaea3b44db5d666e7bbc1a1c6
rajue3 <30584834+rajue3@users.noreply.github.com>
2025-03-03 12:31:52 +0530

    STOCK REALTED CHANGES

--- BELL_UPD_ITEMS_PRATE_MINORDER.sql
+++ BELL_UPD_ITEMS_PRATE_MINORDER.sql
@@ -1,14 +1,17 @@
+select * from Bell_ItemMaster where itemcode in (16,17)
 
 --TO UPDATE PURCHASE RATE AND MINIMUM ORDER ALERT VALUE FROM WEB APP.
 ALTER PROCEDURE  BELL_UPD_ITEMS_PRATE_MINORDER  
-@ITEMCODE AS INTEGER,
+@ITEMCODE AS INTEGER,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 @PRATE AS MONEY,  
 @MINORDERALERT AS INTEGER,  
+@STOCK AS INTEGER,
 @USERNAME AS VARCHAR(30)      
 AS                   
 BEGIN          
   
     UPDATE Bell_ItemMaster SET PRATE=@PRATE, MINORDERALERT=@MINORDERALERT,USERNAME=@USERNAME,ActionDate=GETDATE()      
+	,STOCK=@STOCK
     WHERE ITEMCODE=@ITEMCODE    -- AND ITEMNAME=@ITEMNAME
         
 END   
--- USP_GET_AREALIST.sql
+++ USP_GET_AREALIST.sql
@@ -1,43 +1,45 @@
--- USP_GET_AREALIST 'users'
--- USP_GET_AREALIST 'category'
--- USP_GET_AREALIST 'lines'
--- USP_GET_AREALIST 'area'
--- USP_GET_AREALIST 'BHADRACHALAM'
--- USP_GET_AREALIST 'ETURNAGARAM'
-alter procedure USP_GET_AREALIST 
-@Type as varchar(20) = null,
-@FROMDATE AS DATE = null,    
-@TODATE AS DATE = null    
-AS
-Begin
-  IF (lower(@Type) = 'users')
-	Begin
-    print 'its Item Categories'
-		Select Distinct USERNAME as LINE,'' as Area,'' as ShopName,'' as CustomerName from Bell_USERS where Status='display'
-	end
-  else IF (lower(@Type) = 'category')
-	Begin
-    print 'its Item Categories'
-		Select Distinct CATEGORY as LINE,'' as Area,'' as ShopName,'' as CustomerName  from Bell_ItemMaster --where Status='Active'
-	end
-	else IF (lower(@Type) = 'lines')
-	Begin
-    print 'its line'
-		Select Distinct Line,'' as Area,'' as ShopName,'' as CustomerName  from Bell_Cust_Master where Status='Active' and isnull(Line,'') <> '' order by Line
-	end
-	else
-	IF (isnull(@Type,'') = '' OR @Type = 'area')
-	Begin
-    print 'its area'
-		Select Distinct Area,'' as Line,'' as ShopName,'' as CustomerName  from Bell_Cust_Master where Status='Active' and isnull(Area,'') <> '' order by Area
-	end
-	else
-	begin
-      print 'its shops'
-		Select Distinct ShopName,CustomerName,'' as Area,'' as Line from Bell_Cust_Master where Status='Active' and Line=@Type  order by ShopName
-    --select distinct ShopName, '' as Area,'' as CustomerName,'' as Line FROM bhavani_ER_Bills A    
-    --WHERE Area=@Type AND (CONVERT(varchar(10),BILLDATE,102) Between CONVERT(varchar(10),@FROMDATE,102) and CONVERT(varchar(10),@TODATE,102))    
-    --order by ShopName   
-	end
-End
-GO
+  
+-- USP_GET_AREALIST 'users'  
+-- USP_GET_AREALIST 'category'  
+-- USP_GET_AREALIST 'lines'  
+-- USP_GET_AREALIST 'area'  
+-- USP_GET_AREALIST ''  
+-- USP_GET_AREALIST 'BHADRACHALAM'  
+-- USP_GET_AREALIST 'ASHWAPUR'  
+alter procedure USP_GET_AREALIST   
+@Type as varchar(20) = null,  
+@FROMDATE AS DATE = null,      
+@TODATE AS DATE = null      
+AS  
+Begin  
+  IF (lower(@Type) = 'users')  
+ Begin  
+    print 'its Item Categories'  
+  Select Distinct USERNAME as LINE,'' as Area,'' as ShopName,'' as CustomerName from Bell_USERS where Status='display'  
+ end  
+  else IF (lower(@Type) = 'category')  
+ Begin  
+    print 'its Item Categories'  
+  Select Distinct CATEGORY as LINE,'' as Area,'' as ShopName,'' as CustomerName  from Bell_ItemMaster --where Status='Active'  
+ end  
+ else IF (lower(@Type) = 'lines')  
+ Begin  
+    print 'its line'  
+  Select Distinct Line,'' as Area,'' as ShopName,'' as CustomerName  from Bell_Cust_Master where Status='Active' and isnull(Line,'') <> '' order by Line  
+ end  
+ else  
+ IF (isnull(@Type,'') = '' OR @Type = 'area')  
+ Begin  
+    print 'its area'  
+  Select Distinct Area,Line,'' as ShopName,'' as CustomerName  from Bell_Cust_Master where Status='Active' and isnull(Area,'') <> ''  order by Area  
+ end  
+ else  
+ begin  
+      print 'its shops'  
+  Select Distinct ShopName,CustomerName,'' as Area,'' as Line from Bell_Cust_Master where Status='Active' and Area=@Type  order by ShopName  
+  --Select Distinct ShopName,CustomerName,'' as Area,'' as Line from Bell_Cust_Master where Status='Active' and Line=@Type  order by ShopName  
+    --select distinct ShopName, '' as Area,'' as CustomerName,'' as Line FROM bhavani_ER_Bills A      
+    --WHERE Area=@Type AND (CONVERT(varchar(10),BILLDATE,102) Between CONVERT(varchar(10),@FROMDATE,102) and CONVERT(varchar(10),@TODATE,102))      
+    --order by ShopName     
+ end  
+End  
